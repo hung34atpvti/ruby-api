233c113277f6f6b7180f2e3b5b146fb570b3c723
hung34atpvti <hung34atpvti@gmail.com>
2023-09-12 16:49:12 +0700

    jwt config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  include JwtToken
+
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    authorization = requests[:authorization]
+    token = authorization.split("Bearer ").last if authorization.nil?
+    if token.nil?
+      begin
+        @decoded = JwtToken.decode(token)
+        @current_user = User.find(@decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
+      end
+    else
+      render json: { errors: "Empty token" }, status: :unauthorized
+    end
+  end
 end
---
+++ app/controllers/concerns/jwt_token.rb
@@ -0,0 +1,16 @@
+require "jwt"
+
+module JwtToken
+  extend ActiveSupport::Concern
+  SECRET_KEY = Rails.application.credentials.jwt_secret_key.to_s
+
+  def jwt_endcode(payload, exp: Rails.application.credentials.jwt_expired.to_i.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def jwt_decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new(decoded)
+  end
+end
