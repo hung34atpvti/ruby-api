8a92cfbe0767236df8508bfef3eaf6817a950584
hung34atpvti <hung34atpvti@gmail.com>
2023-09-12 16:57:23 +0700

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user, only: [:create]
+  before_action :find_user, only: [:show, :update, :destroy]
+
+  def index
+    @users = User.all
+    render json: @users, status: 200
+  end
+
+  def show
+    render json: @user, status: 200
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: 200
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
